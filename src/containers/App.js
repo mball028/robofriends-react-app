744992083060a7f4bb8e5fa459dab97ae6529d1d
Michael <mball.28@outlook.com>
2019-05-21 11:01:10 -0600

    redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 import './app.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 
 // we can create and export our App Component at the same time
-export default class App extends Component {
+class App extends Component {
 
     //setting our state
     constructor(){
@@ -14,7 +29,6 @@ export default class App extends Component {
         //state is set as an object
         this.state = {
             robots: [],
-            searchfield: ''
         }
     }
 
@@ -29,39 +43,42 @@ export default class App extends Component {
         .then(users => this.setState({ robots: users }))
     }
 
-    onSearchChange = (e) => {
-        //searchfield state gets changed when value is input
-        this.setState({ searchfield: e.target.value })
-    }
 
     render() {
         //we set = this.state to refactor our code DRY
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         //filter out robot names with characters matching searchfield
         const filteredRobots = robots.filter(robot => {
             // use toLowerCase to the char values are ===
             // .includes() will see if the values from searchfield are in robot.filter(robot.name)
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
         //using a ternary we can return Loading... or cardlist inside the scroll component
-        return !robots.length ?
-            // if we are waiting for a lot of data and it takes time to render
-            <h2 className='light-red pa2 tc'>Loading...</h2> :
-            (
-                <div className='tc'>
-                    <h1 className='f1 light-red grow'>RoboFriends</h1>
-                    {/* when we type in search we call onSearchChange function */}
-                    <SearchBox searchChange={this.onSearchChange} />
-                    {/* we can pass props via children components */}
-                    <Scroll>
-                        {/* pass props as filter robots from our variable above to cardList component */}
-                        <CardList robots={filteredRobots} />
-                    </Scroll>
+        return !robots.length ? (
+          // if we are waiting for a lot of data and it takes time to render
+          <h2 className="light-red pa2 tc">Loading...</h2>
+        ) : (
+          <div className="tc">
+            <h1 className="f1 light-red grow">RoboFriends</h1>
+            {/* when we type in search we call onSearchChange function */}
+            <SearchBox searchChange={onSearchChange} />
+            {/* we can pass props via children components */}
+            <Scroll>
+                {/* if anything in the cardlist fails, it is going to catch and display error message */}
+              <ErrorBoundary>
+                {/* pass props as filter robots from our variable above to cardList component */}
+                <CardList robots={filteredRobots} />
+              </ErrorBoundary>
+            </Scroll>
 
-                    <h5 className="light-red code">Created by Michael!</h5>
-                </div>
-            )
+            <h5 className="light-red code">Created by Michael!</h5>
+          </div>
+        );
 
     }
 }
 
+
+// a higher order function returns that returns another function
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { Store, createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App'
 import './index.css';
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App  />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
